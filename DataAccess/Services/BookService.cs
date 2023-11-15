023c7d8991e36b55533b224ae01f358bb2066c17
thiennlse <thiennlse172858@gmail.com>
2023-11-15 20:04:44 +0700

    Update Services in DataAccess

---
+++ DataAccess/Services/BookService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Business.Repositories;
+
+namespace DataAccess.Services
+{
+    public class BookService
+    {
+        private BookRepository repo = new BookRepository();
+
+        public List<Book> GetAllBookID()
+        {
+            return repo.GetAll();
+        }
+
+        public List<Book> SearchBooks(string keyword)
+        {
+            return repo.GetAll().Where(b => b.BookName.ToLower().Contains(keyword.ToLower()) ||
+                                             b.BookDescription.ToLower().Contains(keyword.ToLower())).ToList();
+        }
+
+        public void DeleteABook(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Book? GetABook(int id)
+        {
+            return repo.Get(id);
+        }
+
+        public void AddABook(Book book)
+        {
+            repo.Create(book);
+        }
+
+        public void UpdateABook(Book book)
+        {
+            repo.Update(book);
+        }
+    }
+}
